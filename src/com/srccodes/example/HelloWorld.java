9e5832127556cfe44502c1c2f6c84f47e5126ae3
ntoni83 <36699770+ntoni83@users.noreply.github.com>
2018-02-26 10:56:16 +0100

    Aggiunta nuova riga e nuova classe

--- src/com/srccodes/example/HelloWorld.java
+++ src/com/srccodes/example/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Aggiunta nuova riga per il branch");
+        System.out.println("Secondo branch con anche nuova classe");
     }
 
 }
